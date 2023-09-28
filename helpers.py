09c9a7db904f9eafd5290d792769e6ea329a0b96
tomer.ronen <tomer.ronen@deci.ai>
2023-09-28 13:09:10 +0300

    to_2tuple from helpers.py

---
+++ helpers.py
@@ -0,0 +1,16 @@
+""" Taken from timm """
+
+import collections
+from itertools import repeat
+
+
+# From PyTorch internals
+def _ntuple(n):
+    def parse(x):
+        if isinstance(x, collections.abc.Iterable):
+            return x
+        return tuple(repeat(x, n))
+    return parse
+
+
+to_2tuple = _ntuple(2)
